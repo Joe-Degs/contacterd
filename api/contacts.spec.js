6122bdedff1dc370048fb40de176d44d58ee255b
Joe-Degs <jpedegs8990@gmail.com>
2020-05-12 16:16:05 +0000

    [Refactoring] clearing spaces in code

--- api/contacts.spec.js
+++ api/contacts.spec.js
@@ -1,8 +1,10 @@
 /* eslint-env mocha */
 const request = require('supertest');
+const test = require('assert');
 const server = require('../server/server');
 
 describe('[ MOVIES API ]', () => {
+  let importantObject = null;
   let app = null;
   let testContacts = [{
     id: 'jdfjdjfdkjkdjdikd',
@@ -34,7 +36,7 @@ describe('[ MOVIES API ]', () => {
       email: contact.email,
       gender: contact.gender,
       phone_number: contact.phone_number,
-      uri: `/api/v1/contacterd/get/${contact._id}`
+      url: `/api/v1/contacterd/get/${contact.id}`
     }
   }
 
@@ -42,18 +44,18 @@ describe('[ MOVIES API ]', () => {
     getAllContacts ()  {
       const res = {
         count: testContacts.length,
-        contacts: contacts.map(pick)
+        contacts: testContacts.map(pick)
       }
       return Promise.resolve(res);
     },
-    getContact (ID) {
-      const contact = testContacts.find(({ id }) => ID === id);
+    getContact (Id) {
+      const contact = testContacts.find(({ id }) => Id === id);
       return Promise.resolve(...[contact].map(pick));
     },
     addContact (body) {
       body.id = 'jfdkfjirierdjfkdjfkei';
       const res = testContacts.push(body);
-      return Promise.resolve(...[body].map(pick))
+      return Promise.resolve(...[body].map(pick));
     },
     updateContact (Id, body) {
       const arrItemIdx = testContacts.findIndex(({ id }) => id === Id);
@@ -85,30 +87,79 @@ describe('[ MOVIES API ]', () => {
     request(app)
       .get('/api/v1/contacterd/get')
       .expect(res => {
-        res.body.contacts.should.containEql({
-          'first_name': 'Lupez',
-          'last_name': 'Marcieli',
-          'email': 'ureirj@gmail.com',
-          'gender': 'Female',
-          'phone_number': '92039384'
-          'url': '/api/v1/contacterd/get/hdfdhfuihuerhuhdfjdhf'
-        })
-        .expect(200, done)
+        const exp = {
+          count: testContacts.length,
+          contacts: testContacts.map(pick)
+        }
+        importantObject = res.body.contacts;
+        test.strict.deepEqual(res.body.contacts, exp.contacts);
       })
+      .expect(200, done)
   });
 
-  it('should return one contact', (done) => {
+  it('should add one contact and return it', (done) => {
+    const exp = {
+      id: 'jfdkfjirierdjfkdjfkei',
+      first_name: 'Joe',
+      last_name: 'Screen',
+      email: 'tmlail@umail.com',
+      gender: 'Male',
+      phone_number: '343434334'
+    };
+
     request(app)
       .post('/api/v1/contacterd/post')
-      .send({
-        first_name: 'Joe',
-        last_name: 'Screen',
-        email: 'tmlail@umail.com',
-        gender: 'Male',
-        phone_number: '343434334'
-      })
+      .send(exp)
       .expect(res => {
-        res.body
+        test.strict.deepEqual(res.body, ...[exp].map(pick));
+        test.strict.equal(testContacts.length, importantObject.length + 1);
       })
+      .expect(201, done)
   });
+
+  it('should return an item with the specified id', (done) => {
+    request(app)
+      .get('/api/v1/contacterd/get/oiroeirodkdkfdfijier')
+      .expect(res => {
+        test.strict.deepEqual({
+          first_name: 'Benard',
+          last_name: 'Kofi_son',
+          email: 'jfeifjeifj@tmail.com',
+          gender: 'Male',
+          phone_number: '9393394884',
+          'url': '/api/v1/contacterd/get/oiroeirodkdkfdfijier'
+        }, res.body)
+      })
+      .expect(200, done)
+  });
+
+  it('should update contact with specified id', (done) => {
+    const obj = {
+      first_name: 'Joshua',
+      last_name: 'Clottey'
+    };
+
+    request(app)
+      .put('/api/v1/contacterd/update/jdfjdjfdkjkdjdikd')
+      .send(obj)
+      .expect(res => {
+        const exp = testContacts[0];
+        Object.keys(obj).forEach(key => exp[key] = obj[key]);
+        test.strict.deepEqual(res.body, ...[exp].map(pick));
+      })
+      .expect(200, done);
+  });
+
+  it('should delete an item from the database and return it', (done) => {
+    const exp = JSON.stringify(testContacts[1]);
+    request(app)
+      .delete('api/v1/contacterd/delete/oiroeirodkdkfdfijier')
+      .expect(res => {
+        test.strict.deepEqual(res.body, ...[JSON.parse(exp)].map(pick));
+        test.strict.notEqual(testContacts.length, importantObject.length);
+        test.strict.equal(importantObject.length - 1, testContacts.length);        
+      })
+      .expect(200, done)
+  });
+
 });
